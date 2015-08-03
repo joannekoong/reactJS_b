f677f09483e57b613f8474e7420b099f4e950789
joanne <joannexo@users.noreply.github.com>
2015-08-03 16:00:07 -0700

    beginning (obligatory) hello world app

---
+++ helloworld/main.jsx
@@ -0,0 +1,7 @@
+// to use component, invoke React.render() method 
+React.render(
+	// component to render 
+	<Hello />,
+	// DOm element where we want to inject result of component 
+	document.body 
+); 
